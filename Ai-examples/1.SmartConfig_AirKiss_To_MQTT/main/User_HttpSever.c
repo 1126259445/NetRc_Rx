3297d8e38ebb295b1d66c2592c80c4e7ccdad39a
1126259445 <1126259445@qq.com>
2022-03-16 20:27:35 -0700

    增加网页系统重启，修改通道设置的默认值及范围

--- Ai-examples/1.SmartConfig_AirKiss_To_MQTT/main/User_HttpSever.c
+++ Ai-examples/1.SmartConfig_AirKiss_To_MQTT/main/User_HttpSever.c
@@ -60,12 +60,18 @@ char home_html[1500] = {" \
 		</form> \
             <br><br> \
 		<form action=\"safe_ch_set\"> \
-  		失控保护: 通道1<input type=\"number\" name=\"safe_ch1\" min=\"900\" max=\"2000\"> \
-			    通道2<input type=\"number\" name=\"safe_ch2\" min=\"900\" max=\"2000\"> \
-			    通道3<input type=\"number\" name=\"safe_ch3\" min=\"900\" max=\"2000\">	\
-                通道4<input type=\"number\" name=\"safe_ch4\" min=\"900\" max=\"2000\"> \
-			    通道5<input type=\"number\" name=\"safe_ch5\" min=\"900\" max=\"2000\"> \
+  		失控保护: 通道1<input type=\"number\" name=\"safe_ch1\" value= \"1500\" min=\"1000\" max=\"2000\"> \
+			    通道2<input type=\"number\" name=\"safe_ch2\" value= \"1500\" min=\"1000\" max=\"2000\"> \
+			    通道3<input type=\"number\" name=\"safe_ch3\" value= \"1500\" min=\"1000\" max=\"2000\">	\
+                通道4<input type=\"number\" name=\"safe_ch4\" value= \"1500\" min=\"1000\" max=\"2000\"> \
+			    通道5<input type=\"number\" name=\"safe_ch5\" value= \"1500\" min=\"1000\" max=\"2000\"> \
   		<input type=\"submit\" value=\"设置\"> \
+		</form> \
+		<br><br> \
+		系统设置： \
+		<br> \
+		<form action=\"sys_reset\"> \
+  		<input type=\"submit\" value=\"重启\"> \
 		</form> \
 	</body> \
 </html> \
@@ -117,6 +123,13 @@ void NetRc_save_info(char *Device_Mac)
 }
 
 
+
+esp_err_t sys_reset_get_handler(httpd_req_t *req)
+{
+     ESP_LOGI(TAG, "sys_reset_get_handler");
+     esp_restart();
+     return ESP_OK;
+}
 
 esp_err_t safe_get_handler(httpd_req_t *req)
 {
@@ -264,6 +277,15 @@ esp_err_t home_get_handler(httpd_req_t *req)
     return ESP_OK;
 }
 
+httpd_uri_t sys_reset ={
+    .uri       = "/sys_reset",
+    .method    = HTTP_GET,
+    .handler   = sys_reset_get_handler,
+    /* Let's pass response string in user
+     * context to demonstrate it's usage */
+//    .user_ctx  = home_html
+};
+
 httpd_uri_t safe = {
     .uri       = "/safe_ch_set",
     .method    = HTTP_GET,
@@ -305,6 +327,7 @@ httpd_handle_t start_webserver(void)
         httpd_register_uri_handler(server, &home);
         httpd_register_uri_handler(server, &wifi);
         httpd_register_uri_handler(server, &safe);
+        httpd_register_uri_handler(server, &sys_reset);
         return server;
     }
 
