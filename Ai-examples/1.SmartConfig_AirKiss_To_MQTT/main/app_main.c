ca0dc81f217e00f8bd5f10837e7353fb72e23245
1126259445 <1126259445@qq.com>
2022-03-19 06:11:39 -0700

    false reconnect

--- Ai-examples/1.SmartConfig_AirKiss_To_MQTT/main/app_main.c
+++ Ai-examples/1.SmartConfig_AirKiss_To_MQTT/main/app_main.c
@@ -180,7 +180,7 @@ void TaskXMqttRecieve(void *p)
 		.client_id = deviceUUID,
 		.event_handle = MqttCloudsCallBack, //设置回调函数
 		.keepalive = 120,					//心跳
-		.disable_auto_reconnect = true,	//开启自动重连
+		.disable_auto_reconnect = false,	//开启自动重连
 		.disable_clean_session = false,		//开启 清除会话
 		.buffer_size = 1024*5,
 	};
